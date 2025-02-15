44f21023f8f745e45f096ac4f2746ad1781f75b5
Kittipat <first1801@hotmail.com>
2025-02-15 23:44:22 +0700

    fix Validation not working

--- src/main/java/com/eshop/service/order_product_service/dto/CreateProductRequest.java
+++ src/main/java/com/eshop/service/order_product_service/dto/CreateProductRequest.java
@@ -3,29 +3,27 @@ package com.eshop.service.order_product_service.dto;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.Getter;
 
 
 import java.math.BigDecimal;
 
 @Data
 public class CreateProductRequest {
-    @NotNull(message = "Product name cannot be null")
-    @Column(nullable = false)
+    @NotBlank(message = "description cannot be empty")
     private String name;
 
-    @NotNull(message = "description name cannot be null")
-    @Column(nullable = false)
+    @NotBlank(message = "description cannot be empty")
     private String description;
 
-    @NotNull(message = "price name cannot be null")
-    @Column(nullable = false)
+    @NotNull(message = "stock name cannot be null")
+    @Min(value = 1, message = "Stock must be at least 1")
+    @Digits(integer = 10, fraction = 2, message = "Stock must be a number")
     private BigDecimal price;
 
     @NotNull(message = "stock name cannot be null")
-    @Column(nullable = false)
     @Min(value = 1, message = "Stock must be at least 1")
     @Digits(integer = 10, fraction = 0, message = "Stock must be a number")
     private Integer stock;
